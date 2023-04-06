015ffc1fd5b0c2305f9dd5dd5f61978b71da4782
Douglas Dreer <douglasdreer@outlook.com.br>
2023-04-06 12:11:29 -0300

    Melhoria de conversão

---
+++ src/main/java/org/projetosetevidas/gerenciamentopets/dominio/utils/MapperUtil.java
@@ -0,0 +1,26 @@
+package org.projetosetevidas.gerenciamentopets.dominio.utils;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Component
+public class MapperUtil {
+    private static final ModelMapper modelMapper = new ModelMapper();
+
+    public static <D> D convertTo(Object bean, Class<D> dto) {
+        return modelMapper.map(bean, dto);
+    }
+
+    public static <S, T> List<T> mapList(List<S> source, Class<T> targetClass) {
+        return source
+                .stream()
+                .map(element -> modelMapper.map(element, targetClass))
+                .collect(Collectors.toList());
+    }
+
+
+}
