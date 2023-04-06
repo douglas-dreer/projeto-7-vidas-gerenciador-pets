5673fc8a21aadfda18e598c24bb19234cecb6c0c
Douglas Dreer <douglasdreer@outlook.com.br>
2023-04-06 17:51:26 -0300

    Alterado a url

--- src/main/java/org/projetosetevidas/gerenciamentopets/aplicacao/adptadores/controles/GatoController.java
+++ src/main/java/org/projetosetevidas/gerenciamentopets/aplicacao/adptadores/controles/GatoController.java
@@ -11,7 +11,7 @@ import java.net.URISyntaxException;
 import java.util.List;
 
 @RestController
-@RequestMapping("cadastro/gato")
+@RequestMapping("gerenciamento/gatos")
 public class GatoController {
     private final GatoServicePort gatoServicePort;
     private final HttpServletRequest httpServletRequest;
