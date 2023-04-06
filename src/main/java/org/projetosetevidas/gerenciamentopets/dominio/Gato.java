0e1ce64267cd8eb20f98353e64d0e6fed23da980
Douglas Dreer <douglasdreer@outlook.com.br>
2023-04-06 13:31:42 -0300

    Remoção de construtor sem uso

--- src/main/java/org/projetosetevidas/gerenciamentopets/dominio/Gato.java
+++ src/main/java/org/projetosetevidas/gerenciamentopets/dominio/Gato.java
@@ -13,18 +13,4 @@ import org.projetosetevidas.gerenciamentopets.dominio.enums.RacaGatoEnum;
 @Data
 public class Gato extends Pet{
     private RacaGatoEnum raca;
-
-    public Gato(GatoDTO dto) {
-        setId(dto.getId());
-        setNome(dto.getNome());
-        setIdade(dto.getIdade());
-        setRaca(dto.getRaca());
-        setDataNascimento(dto.getDataNascimento());
-        setVacinado(dto.isVacinado());
-        setCastrado(dto.isCastrado());
-        setAdotado(dto.isAdotado());
-        setObservacao(dto.getObservacao());
-        setDataInclusao(dto.getDataInclusao());
-        setDataModificacao(dto.getDataModificacao());
-    }
 }
